fb4b737031173f068fcb2ec652fcd95d66fe6dea
magico <magicocarella@gmail.com>
2020-06-15 00:22:31 +0100

    v1.0.1

--- src/app/components/pages/footer/footer.component.ts
+++ src/app/components/pages/footer/footer.component.ts
@@ -13,4 +13,30 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scroll(element: any) {
+    console.log(element);
+    switch (element) {
+      case 'somos':
+        element = document.querySelector('#somos');
+        break;
+      case 'slider':
+        element = document.querySelector('#slider');
+        break;
+      case 'clientes':
+        element = document.querySelector('#clientes');
+        break;
+      case 'categorias':
+        element = document.querySelector('#categorias');
+        break;
+      case 'contacto':
+        element = document.querySelector('#contacto');
+        break;
+      default:
+        break;
+    }
+
+    setTimeout(() => element.scrollIntoView({behavior: 'smooth'}), 100);
+    // element.scrollIntoView({ behavior: 'smooth' });
+    // element = null;
+  }
 }
